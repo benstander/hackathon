96fb4f46ab33665f12919e7f49d458b452d19fea
Leon <111322217+chimichungus32@users.noreply.github.com>
2025-05-25 16:56:35 +1000

    cors fix

--- backend/middleware.js
+++ backend/middleware.js
@@ -8,8 +8,7 @@ function setupAppMiddleware(app){
   app.use(morgan("dev"))
 
   app.use(cors({
-    origin: process.env.FRONTEND_ORIGIN || "http://localhost:5175" || "http://localhost:5174" || "http://localhost:5173",
-    credentials: true,
+    origin: '*',
   }));
 
   app.use(express.json());
